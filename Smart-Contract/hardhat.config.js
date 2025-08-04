1f6820617fbb93123dcbc43d4f9cfb8aaeebf5f9
ngthcuong <nguyenthanh8a2@gmail.com>
2025-08-04 23:50:20 +0700

    feat: configure hardhat for sepolia network

--- Smart-Contract/hardhat.config.js
+++ Smart-Contract/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 20000000000, // 20 gwei
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
