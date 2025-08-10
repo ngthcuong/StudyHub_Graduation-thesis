1089fd0e0305da3bb4d43b335a6c3c01779d35b7
ngthcuong <nguyenthanh8a2@gmail.com>
2025-08-11 01:11:32 +0700

    feat: add Certificate component and update routing with new path

--- FE-Web-StudyHub/src/routes/index.jsx
+++ FE-Web-StudyHub/src/routes/index.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import AuthCertificate from "../pages/AuthCertificate";
+import Certificate from "../components/Certificate";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AuthCertificate />,
+  },
+  {
+    path: "/certificate",
+    element: <Certificate />,
   },
 ]);
