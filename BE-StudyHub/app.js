2235e721fa44c893db260f6c39bd8a01f347646d
ngthcuong <nguyenthanh8a2@gmail.com>
2025-08-11 15:27:35 +0700

    feat: config connect to mongoDB

---
+++ BE-StudyHub/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const connectToDB = require("./src/configs/database");
+
+require("dotenv").config();
+
+// Create Expresss App and HTTP Server
+const app = express();
+const server = http.createServer(app);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Config CORS
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Connect to MongoDB and start server
+const startServer = async () => {
+  try {
+    await connectToDB(); // Connect to MongoDB
+
+    // Define routes
+    app.get("/", (req, res) => {
+      res.status(200).json({ message: "Welcome to StudyHub API!" });
+    });
+
+    // Handle 404 errors
+    app.use((req, res, next) => {
+      res.status(404).json({ error: "Route not found" });
+    });
+
+    // Global error handler
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).json({ error: "Something went wrong!" });
+    });
+
+    // Start server
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); // Escape when there is an error
+  }
+};
+
+startServer();
---
+++ BE-StudyHub/src/configs/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectToDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDB;
