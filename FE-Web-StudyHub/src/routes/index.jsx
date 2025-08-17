ba67018d192b730ead34e3bcc33e4fe0da3b1c01
ngthcuong <nguyenthanh8a2@gmail.com>
2025-08-17 23:25:07 +0700

    refactor:  update routing structure to include new authentication pages

--- FE-Web-StudyHub/src/routes/index.jsx
+++ FE-Web-StudyHub/src/routes/index.jsx
@@ -1,14 +1,51 @@
 import { createBrowserRouter } from "react-router-dom";
 import AuthCertificate from "../pages/AuthCertificate";
 import Certificate from "../components/Certificate";
+import RootLayout from "../layouts/RootLayout";
+import ProtectedLayout from "../layouts/ProtectedLayout";
+import AuthLayout from "../layouts/AuthLayout";
+import RegisterPage from "../pages/auth/RegisterPage";
+import LoginPage from "../pages/auth/LoginPage";
+import HomgePage from "../pages/HomgePage";
 
 export const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <AuthCertificate />,
+  // },
+  // {
+  //   path: "/certificate",
+  //   element: <Certificate />,
+  // },
   {
-    path: "/",
-    element: <AuthCertificate />,
-  },
-  {
-    path: "/certificate",
-    element: <Certificate />,
+    element: <RootLayout />,
+    children: [
+      {
+        element: <ProtectedLayout />,
+        children: [
+          {
+            path: "/",
+            element: <HomgePage />,
+          },
+          {
+            path: "/verify-cert",
+            element: <Certificate />,
+          },
+        ],
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/register",
+            element: <RegisterPage />,
+          },
+          {
+            path: "/login",
+            element: <LoginPage />,
+          },
+        ],
+      },
+    ],
   },
 ]);
