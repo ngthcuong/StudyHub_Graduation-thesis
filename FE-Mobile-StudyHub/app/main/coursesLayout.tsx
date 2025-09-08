8a3241369bceb8838f78dce97c615db51315ca60
ngthcuong <nguyenthanh8a2@gmail.com>
2025-09-08 22:25:22 +0700

    Remove FE-Mobile-StudyHub project files including package.json, reset-project.js, tsconfig.json, and package-lock.json to clean up the repository.

--- FE-Mobile-StudyHub/app/main/coursesLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function CoursesLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="../courses/index" />
-    </Stack>
-  );
-}
