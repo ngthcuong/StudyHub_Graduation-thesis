cd1332ce2d6d748be4be26e6b4db47d386dc16b7
ngthcuong <nguyenthanh8a2@gmail.com>
2025-09-13 15:26:52 +0700

    refactor:  login functionality to use RTK Query and improve error handling

--- FE-Web-StudyHub/src/services/authApi.js
+++ FE-Web-StudyHub/src/services/authApi.js
@@ -1,95 +1,96 @@
-import axios from "axios";
-import config from "../configs/config";
+// import axios from "axios";
+// import config from "../configs/config";
 import { rootApi } from "./rootApi";
+import { login } from "../redux/slices/auth";
 
-const login = async ({ email, password }) => {
-  try {
-    console.log({ email, password });
+// const login = async ({ email, password }) => {
+//   try {
+//     console.log({ email, password });
 
-    const response = await axios.post(`${config.baseApiUrl}/auth/login`, {
-      email,
-      password,
-    });
+//     const response = await axios.post(`${config.baseApiUrl}/auth/login`, {
+//       email,
+//       password,
+//     });
 
-    console.log(response);
+//     console.log(response);
 
-    return response;
-  } catch (error) {
-    console.error("Login failed: ", error);
+//     return response;
+//   } catch (error) {
+//     console.error("Login failed: ", error);
 
-    // Log more detailed error information
-    if (error.response) {
-      console.error("Error status:", error.response.status);
-      console.error("Error data:", error.response.data);
-    }
+//     // Log more detailed error information
+//     if (error.response) {
+//       console.error("Error status:", error.response.status);
+//       console.error("Error data:", error.response.data);
+//     }
 
-    throw error;
-  }
-};
+//     throw error;
+//   }
+// };
 
-const register = async (data) => {
-  try {
-    console.log("data register: ", {
-      data,
-    });
+// const register = async (data) => {
+//   try {
+//     console.log("data register: ", {
+//       data,
+//     });
 
-    const response = await axios.post(
-      `${config.baseApiUrl}/auth/register`,
-      data
-    );
+//     const response = await axios.post(
+//       `${config.baseApiUrl}/auth/register`,
+//       data
+//     );
 
-    console.log("res in service: ", response);
-    return response;
-  } catch (error) {
-    console.error("Register failed: ", error);
+//     console.log("res in service: ", response);
+//     return response;
+//   } catch (error) {
+//     console.error("Register failed: ", error);
 
-    // Log more detailed error information
-    if (error.response) {
-      console.error("Error status:", error.response.status);
-      console.error("Error data:", error.response.data);
-    }
+//     // Log more detailed error information
+//     if (error.response) {
+//       console.error("Error status:", error.response.status);
+//       console.error("Error data:", error.response.data);
+//     }
 
-    throw error;
-  }
-};
+//     throw error;
+//   }
+// };
 
-const changePassword = async ({ currentPassword, newPassword }) => {
-  try {
-    const accessToken = localStorage.getItem("accessToken");
-    const response = await axios.post(
-      `${config.baseApiUrl}/auth/change-password`,
-      { currentPassword, newPassword },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    if (response.status === 200) {
-      return response.data.message;
-    } else {
-      return response.data.error;
-    }
-  } catch (error) {
-    console.error("Change password failed: ", error);
+// const changePassword = async ({ currentPassword, newPassword }) => {
+//   try {
+//     const accessToken = localStorage.getItem("accessToken");
+//     const response = await axios.post(
+//       `${config.baseApiUrl}/auth/change-password`,
+//       { currentPassword, newPassword },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${accessToken}`,
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     if (response.status === 200) {
+//       return response.data.message;
+//     } else {
+//       return response.data.error;
+//     }
+//   } catch (error) {
+//     console.error("Change password failed: ", error);
 
-    // Log more detailed error information
-    if (error.response) {
-      console.error("Error status:", error.response.status);
-      console.error("Error data:", error.response.data);
-    }
+//     // Log more detailed error information
+//     if (error.response) {
+//       console.error("Error status:", error.response.status);
+//       console.error("Error data:", error.response.data);
+//     }
 
-    throw error;
-  }
-};
+//     throw error;
+//   }
+// };
 
 export const authApi = rootApi.injectEndpoints({
   endpoints: (builder) => ({
     // Đăng nhập
     login: builder.mutation({
       query: ({ email, password }) => ({
-        url: "/atuh/login",
+        url: "/auth/login",
         method: "POST",
         body: { email, password },
       }),
@@ -107,7 +108,7 @@ export const authApi = rootApi.injectEndpoints({
     refreshToken: builder.mutation({
       query: (refreshToken) => ({
         url: "/auth/refresh",
-        metho: "POST",
+        method: "POST",
         body: { refreshToken },
       }),
     }),
@@ -119,5 +120,6 @@ export const authApi = rootApi.injectEndpoints({
   }),
 });
 
-export default { login, register, changePassword };
+// export default { login, register, changePassword };
 export const { useLoginMutation, useRefreshTokenMutation } = authApi;
+export default authApi;
